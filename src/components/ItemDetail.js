5c6d645440bc03a149735b978bd19c969d5420a6
EzequielG <ezequiel.guaymas07@gmail.com>
2022-12-05 20:36:27 -0300

    Routing en funcionamiento, ver comentarios, hacer gif

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,17 +1,39 @@
+import Card from "react-bootstrap/Card";
+import "../App.css";
+
 const ItemDetail = ({ item }) => {
   return item && item.image ? (
-    <div>
-      <div className="item-detail">
-        <div className="item-image"></div>
-        <img src={item.image} alt="Same alt value" />
-        <h3>{item.name}</h3>
-        <p>{item.stock}</p>
-        <p>{item.cost}</p>
-      </div>
-    </div>
+    <Card className="img-size">
+      <Card.Img variant="top" src={item.image} alt="Same alt value" />
+      <Card.Body>
+        <Card.Text>
+          <h3>Titulo: {item.name}</h3>
+          <p>Unidades en stock: {item.stock}</p>
+          <p>Precio x unidad: ${item.cost}</p>
+        </Card.Text>
+      </Card.Body>
+    </Card>
   ) : (
     <p>Cargando detalle...</p>
   );
 };
 
 export default ItemDetail;
+
+// const ItemDetail = ({ item }) => {
+//   return item && item.image ? (
+//     <div>
+//       <div className="item-detail">
+//         <div className="item-image"></div>
+//         <img src={item.image} alt="Same alt value" />
+//         <h3>{item.name}</h3>
+//         <p>{item.stock}</p>
+//         <p>{item.cost}</p>
+//       </div>
+//     </div>
+//   ) : (
+//     <p>Cargando detalle...</p>
+//   );
+// };
+
+// export default ItemDetail;
