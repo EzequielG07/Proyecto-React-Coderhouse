77a3aa339e8319a4f3751be35176ebc153aeab7b
EzequielG <ezequiel.guaymas07@gmail.com>
2022-12-22 14:22:19 -0300

    Se realizo el contador ItemCount y se agrego a ItemDetail

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,7 +1,15 @@
 import Card from "react-bootstrap/Card";
+import ItemCount from "./itemCount"; /*-> PRUEBA DE ItemCount*/
+import { useState } from "react";
 import "../App.css";
 
 const ItemDetail = ({ item }) => {
+  const [itemCount, setItemCount] = useState(0);
+
+  const onAdd = (qty) => {
+    setItemCount(qty);
+  };
+
   return item && item.image ? (
     <Card className="img-size" bg="secondary">
       <Card.Img variant="top" src={item.image} alt="Same alt value" />
@@ -9,7 +17,10 @@ const ItemDetail = ({ item }) => {
         <Card.Text>
           <h3>Titulo: {item.name}</h3>
           <p>Unidades en stock: {item.stock}</p>
-          <p>Precio x unidad: ${item.cost}</p>
+          <p>Precio x unidad: ${item.cost}</p> {/*-> PRUEBA DE ItemCount*/}
+          <div>
+            <ItemCount stock={item.stock} initial={itemCount} onAdd={onAdd} />
+          </div>
         </Card.Text>
       </Card.Body>
     </Card>
---
+++ src/components/itemCount.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+const ItemCount = ({ stock = 0, initial = 1, onAdd }) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setCount(initial);
+  }, [initial]); // si o si puse initial y es []
+
+  const increment = () => {
+    if (count < stock) {
+      setCount(count + 1);
+    }
+  };
+
+  const decrement = () => {
+    if (count > initial + 1) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <>
+      <button onClick={increment}> + </button>
+      {count}
+      <button onClick={decrement}> - </button>
+    </>
+  );
+};
+
+export default ItemCount;
